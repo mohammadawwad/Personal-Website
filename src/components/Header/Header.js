f25243a6842cf26401d98c47723897cf31d7719d
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2022-04-03 07:24:50 +0000

    im tired just need to fix header, check if wwworlks on mobike and fix grid for achievements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,31 +8,32 @@ import CustomizedSwitch from './Switch';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <Container>
-    <Div1>
-      <Link href="/">
-        <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-          <DiCssdeck size="3rem" /> <span>Home</span>
-        </a>
-      </Link>
-    </Div1>
-    <Div2>
-      <li>
-        <Link href="#about">
-          <NavLink>About</NavLink>
+
+    <Container>
+      <Div1>
+        <Link href="/">
+          <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
+            <DiCssdeck size="3rem" /> <span>Home</span>
+          </a>
         </Link>
-      </li>    
-      <li>
-        <Link href="#tech">
-          <NavLink>Technologies</NavLink>
-        </Link>
-      </li>    
-      <li>
-        <Link href="#projects">
-          <NavLink>Projects</NavLink>
-        </Link>
-      </li>       
-    </Div2>
+      </Div1>
+      <Div2>
+        <li>
+          <Link href="#about">
+            <NavLink>About</NavLink>
+          </Link>
+        </li>    
+        <li>
+          <Link href="#tech">
+            <NavLink>Technologies</NavLink>
+          </Link>
+        </li>    
+        <li>
+        <  Link href="#projects">
+            <NavLink>Projects</NavLink>
+          </Link>
+        </li>       
+      </Div2>
       <Div3>
         <SocialIcons target="_blank" href="https://github.com/mohammadawwad">
           <AiFillGithub size="3rem" />
@@ -49,6 +50,7 @@ const Header = () =>  (
         <CustomizedSwitch/>
       </Div3>
     </Container>
+
 );
 
 export default Header;
--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -1,6 +1,7 @@
 import { IoIosArrowDropdown } from 'react-icons/io';
 import styled from 'styled-components';
 
+
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
