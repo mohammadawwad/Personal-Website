af4d3bb8c66742b8e92ab8e8db8d360e462bc574
Mohammad Awwad <48971400+mohammadawwad@users.noreply.github.com>
2022-05-17 01:45:59 +0000

    small update working on an easter egg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,17 +5,39 @@ import { SiReplit } from 'react-icons/si';
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
+
+let showEasterEgg = false;
+
+const Popup = () => {
+    showEasterEgg = true;
+}
+
+
 const Footer = () => {
   return (
     <FooterWrapper>
+
+      {/*Email Section*/}
       <LinkList>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
           <LinkItem style={{width: '300px'}} target="_blank" href="mailto:mohd-awwad@hotmail.com">
             mohd-awwad@hotmail.com
           </LinkItem>
+
+          <LinkItem onClick={Popup()}>
+            {/*Change lock to an actual lock logo*/}
+            Lock
+
+            {/*prompt for an input and if it is correct output the image*/}
+            <img  width="96" height="65" src="/images/headshot.png"/>
+              
+          </LinkItem>
         </LinkColumn>
+
       </LinkList>
+
+      {/*Social links and buttons Section*/}
       <SocialIconsContainer>
         <CompanyContainer>
           <Slogan>Innovating one project at a time</Slogan>
